2aa688eadf6baa8a2f93f645a1dc62a634e58e5e
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-05-15 19:42:42 +0200

    Update tests to make use of pytest

--- tests/test_compile.py
+++ tests/test_compile.py
@@ -1,13 +1,5 @@
 
-from unittest import TestCase
-
-try:
-    import astor
-except ImportError:
-    pass
-
-from pyvalidate.decorators import validator
-from pyvalidate.compile import compile_validator
+from pyvalidate import validator, compile_validator
 
 def has_next(iterable):
     try:
@@ -16,71 +8,69 @@ def has_next(iterable):
         return False
     return True
 
-class TestCompile(TestCase):
+def has_same_output(validator, value):
+    return validator.is_valid(value) == (not has_next(validator.get_errors(value)))
 
-    def assert_validates(self, validator, value):
-        self.assertEqual(validator.is_valid(value), not has_next(validator.get_errors(value)))
+def test_simple_equality():
 
-    def test_simple_equality(self):
+    @validator
+    def is_valid(value):
+        return value == 1
 
-        @validator
-        def is_valid(value):
-            return value == 1
+    has_same_output(is_valid, 1)
+    has_same_output(is_valid, 2)
+    has_same_output(is_valid, False)
+    has_same_output(is_valid, "Foo!")
+    print(str(list(is_valid.get_errors("foo"))[0]))
 
-        self.assert_validates(is_valid, 1)
-        self.assert_validates(is_valid, 2)
-        self.assert_validates(is_valid, False)
-        self.assert_validates(is_valid, "Foo!")
-        print(str(list(is_valid.get_errors("foo"))[0]))
+def test_early_return():
 
-    def test_early_return(self):
+    @validator
+    def is_valid(value):
+        if value != 1:
+            return False
+        return True
 
-        @validator
-        def is_valid(value):
-            if value != 1:
-                return False
-            return True
+    has_same_output(is_valid, 1)
+    has_same_output(is_valid, 2)
+    has_same_output(is_valid, False)
+    has_same_output(is_valid, "Foo!")
 
-        self.assert_validates(is_valid, 1)
-        self.assert_validates(is_valid, 2)
-        self.assert_validates(is_valid, False)
-        self.assert_validates(is_valid, "Foo!")
+def test_boolean_or():
 
-    def test_boolean_or(self):
+    @validator
+    def is_valid(value):
+        return value == 1 or value == 2
 
-        @validator
-        def is_valid(value):
-            return value == 1 or value == 2
+    has_same_output(is_valid, 1)
+    has_same_output(is_valid, 2)
+    has_same_output(is_valid, 3)
+    has_same_output(is_valid, False)
+    has_same_output(is_valid, "Foo!")
 
-        self.assert_validates(is_valid, 1)
-        self.assert_validates(is_valid, 2)
-        self.assert_validates(is_valid, 3)
-        self.assert_validates(is_valid, False)
-        self.assert_validates(is_valid, "Foo!")
+def test_readme_example():
 
-    def test_readme_example(self):
+    @validator
+    def valid_password(password):
+        if len(password) < 8:
+            return False
+        return password != 'password'  \
+           and password != 'easypeasy' \
+           and password != 'p@ssw0rd'
 
-        @validator
-        def valid_password(password):
-            if len(password) < 8:
-                return False
-            return password != 'password'  \
-               and password != 'easypeasy' \
-               and password != 'p@ssw0rd'
+    has_same_output(valid_password, 'somelongpassword123')
+    has_same_output(valid_password, 'w3rdp@zzw0rdZw0rkT00')
+    has_same_output(valid_password, 'password')
+    has_same_output(valid_password, 'soshort')
 
-        self.assert_validates(valid_password, 'somelongpassword123')
-        self.assert_validates(valid_password, 'w3rdp@zzw0rdZw0rkT00')
-        self.assert_validates(valid_password, 'password')
-        self.assert_validates(valid_password, 'soshort')
+def test_in_not_in():
 
-    def test_in_not_in(self):
+    @validator
+    def is_valid(value):
+        return value in ['test', 'foo', 'bar']
 
-        @validator
-        def is_valid(value):
-            return value in ['test', 'foo', 'bar']
-
-        msgs = list(is_valid.get_errors('bax'))
-        self.assertEqual(str(msgs[0]), "value must be either of 'test', 'foo' or 'bar'")
-        self.assert_validates(is_valid, 'test')
-        self.assert_validates(is_valid, 'bax')
+    msgs = list(is_valid.get_errors('bax'))
+    assert(str(msgs[0]) == "value must be either of 'test', 'foo' or 'bar'")
+    has_same_output(is_valid, 'test')
+    has_same_output(is_valid, 'bax')
 
