f82c424f412c524da5f5173b0115ba4701e1988c
Sam Vervaeck <vervaeck.sam@skynet.be>
2020-05-15 18:42:03 +0200

    Fix __init__.py

--- pyvalidate/__init__.py
+++ pyvalidate/__init__.py
@@ -1,15 +1,3 @@
 
+from .compile import compile_validator
 
-
-from pyvalidate import ArgumentTypeException,DenialException,MandatoryException,ParameterTypeException,ParameterValueException,validate,validate_parameters,ValidationException
- 
-__version__ = '1.3.1'
-
-__all__ =['ArgumentTypeException',
-          'DenialException',
-          'MandatoryException',
-          'ParameterTypeException',
-          'ParameterValueException',
-          'validate',
-          'validate_parameters',
-          'ValidationException']
